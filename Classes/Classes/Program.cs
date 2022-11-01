b44dc46f2c73bc5783216fd99a84ffd4c0307a42
Uzair Vawda <uzair.vawda@gmail.com>
2022-11-01 12:40:03 -0400

    creating classes, adding constructor, adding getters and setters

--- Classes/Classes/Program.cs
+++ Classes/Classes/Program.cs
@@ -11,11 +11,21 @@ namespace Classes
         static void Main(string[] args)
         {
             Book bookOne = new Book("The First Minute", "Uzair Vawda", 107);
-
             Book bookTwo = new Book("Loss of time", "Loss of time", 99);
-            
-            Console.WriteLine(bookOne.title);
-            Console.WriteLine(bookTwo.title);
+
+            //Console.WriteLine(bookOne.title);
+            //Console.WriteLine(bookTwo.title);
+
+            Student studentOne = new Student("Jane Doe", "CS", 3.5);
+            Student studentTwo = new Student("John Doe", "ENG", 2.6);
+            //Console.WriteLine(studentOne.HasHonors());
+            //Console.WriteLine(studentTwo.HasHonors());
+
+            Movie movieOne = new Movie("Jane Doe", "CS", "PG");
+            Movie movieTwo = new Movie("John Doe", "ENG", "Dog");
+            // G, PG, PG-13, R, NR
+            Console.WriteLine(movieOne.Rating);
+            Console.WriteLine(movieTwo.Rating);
         }
     }
 }
